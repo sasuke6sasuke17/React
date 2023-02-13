a2e937626a3adc44e519a299077eb5df53fa0c25
sasuke6sasuke17 <119826816+sasuke6sasuke17@users.noreply.github.com>
2023-02-13 11:54:10 +0900

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,24 @@
+// import { StrictMode } from "react";
+// import { createRoot } from "react-dom/client";
+
+// import App from "./App";
+
+// const rootElement = document.getElementById("root");
+// const root = createRoot(rootElement);
+
+// root.render(
+//   <StrictMode>
+//     <App />
+//   </StrictMode>
+// );
+
+import { StrictMode } from "react";
+import ReactDom from "react-dom";
+import App from "./App";
+
+ReactDom.render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+  document.getElementById("root")
+);
