7f9d4859011943ba574316af432fbd7040f8d096
sasuke6sasuke17 <119826816+sasuke6sasuke17@users.noreply.github.com>
2023-02-15 10:54:33 +0900

    add

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <>
+      <h1>おはようございます</h1>
+      <p>調子はどうですか</p>
+      <p>ファイルの分割の処理を実行しました。</p>
+    </>
+  );
+};
+//　このファイル以外でApp関数を利用できるよう指定
+export default App;
--- src/index.js
+++ src/index.js
@@ -12,13 +12,49 @@
 //   </StrictMode>
 // );
 
-import { StrictMode } from "react";
+// import { StrictMode } from "react";
+// import ReactDom from "react-dom";
+// import App from "./App";
+
+// ReactDom.render(
+//   <StrictMode>
+//     <App />
+//   </StrictMode>,
+//   document.getElementById("root")
+// );
+
+// reactを使うときの準備として、Reactを読み込む処理が最初に必要
+import React from "react";
 import ReactDom from "react-dom";
 import App from "./App";
 
-ReactDom.render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-  document.getElementById("root")
-);
+// Reactでは関数の中でHTMLタグを書く
+// 出力する内容はretrunの先でHTMLと”ほぼ”同じ構造で記述する
+// 複数のHTMLタグを表示する場合は()を使って記述する
+// divタグで囲むとHTMLの入れ子構造が変わってしまうので＜React.Fragment＞もしくは<>で囲む
+// const App = () => {
+//   return (
+//     // <div>
+//     // <React.Fragment>
+//     <>
+//       <h1>こんにちは</h1>
+//       <p>お元気ですか</p>
+//     </>
+//     // </React.Fragment>
+//     // </div>
+//   );
+// };
+
+// Render関数
+ReactDom.render(<App />, document.getElementById("root")); // Appの閉じタグ的な意味＋IDの場所に関数を入れたい
+
+//　コンポーネント命名規則
+// 正しい例
+// App
+// SomeComponent
+// →頭文字を大文字にする
+
+// 正しくない例
+// app
+// somecompornent
+// →頭文字を小文字にする
